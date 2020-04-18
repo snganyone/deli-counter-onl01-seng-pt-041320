81abba1ef158043f40369e0c61416dcd74c6e186
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-18 07:14:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,6 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
+  puts "Currenty serving"
 end

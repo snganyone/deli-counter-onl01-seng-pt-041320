d088fa40634e5db5d4df0bbba2e8020c6da1b810
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-18 06:33:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 def take_a_number(katz_deli, name)
   arr = []
   katz_deli.each_with_index{|i| i << "{name}"}
-   puts "Welcome, #{name}. You are number in line."
+   puts "Welcome, #{name}. You are number 1 in line."
 end

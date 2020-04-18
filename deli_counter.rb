6adb43ee698a42261e30dbe4a98f83ec795e8628
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-18 07:15:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,8 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  puts "Currenty serving"
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  end
+    
 end
